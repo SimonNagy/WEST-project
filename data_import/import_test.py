8e79794970f021b188ddf8cd8ae1961846988341
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-15 14:05:19 +0200

    Create import_test.py

---
+++ data_import/import_test.py
@@ -0,0 +1,21 @@
+import bme280
+import smbus2
+
+# configuration of hardware ports
+port = 1
+address = "0x76"
+bus = smbus2.SMBus(port)
+
+# loading calibration params and reading a single line of data
+calibration_params = bme280.load_calibration_params(bus, address)
+data = bme280.sample(bus, address, calibration_params)
+
+# compensated reading object returned form above; has the following params
+print(data.id)
+print(data.timestamp)
+print(data.temperature)
+print(data.pressure)
+print(data.humidity)
+
+# string representation of the data
+print(data)
