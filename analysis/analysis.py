7be2076f2f08eb5242fb7c6f9f9a0ddbebe9448d
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-15 14:36:51 +0200

    init architecture

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,17 @@
 import numpy as np
 import pandas as pd
 
+testdata = pd.read_csv("../test_data/data.csv")
+
+class data_objects():
+    def objects(testdata, id, timestamp, temperature, pressure, humidity):
+        id = testdata[0]
+        timestamp = testdata[1]
+        temperature = testdata[2]
+        pressure = testdata[3]
+        humidity = testdata[4]
+
+
 class descriptive_statistics():
 
     def __init__(self, id, timestamp, temperature, pressure, humidity):
@@ -10,5 +21,7 @@ class descriptive_statistics():
         self.pressure = pressure
         self.humidity = humidity
     
-    def descriptive_statistics():
-        avg_
+    def descriptive_statistics(temperature, pressure, humidity):
+        avg_temperature = np.average(temperature)
+        avg_pressure = np.average(pressure)
+        avg_humidity = np.average(humidity)
--- dashboard/display_test.py
+++ dashboard/display_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from ..analysis.analysis import data_objects as do
 
-df = pd.read_csv("../test_data/data.csv")
+print(do)
