635bfc001cc6be27a74a5b0c9697b425d5a8f2f3
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-16 14:41:03 +0200

    fixing display test && app integration

--- console_application/display_test.py
+++ console_application/display_test.py
@@ -1,8 +1,11 @@
-from .. import app
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import app
 
 descriptive_statistics = app.descriptive_statistics
-print(descriptive_statistics)
