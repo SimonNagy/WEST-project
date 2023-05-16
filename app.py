e42838bc453650c4511d5db6948b8e8882fe51fb
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-16 10:54:32 +0200

    pycache

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from data_import import import_test as it
+
 from dashboard import display_test as dt
 from analysis import analysis as an
 
-# integration of the import module to the app module
-data_test = it.data
-print(data_test)
+
 
 # integration of the dashboard module to the app module
 sample_dataframe = dt.df
