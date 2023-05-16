e83b757b2965ceab0a4841db0beb2c17bf1db128
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-16 12:32:56 +0200

    withoutself-objects

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -8,25 +8,32 @@ class data_objects():
 
 class descriptive_statistics():
 
-    def __init__(self, id, timestamp, temperature, pressure, humidity):
-        self.id = id
-        self.timestamp = timestamp
-        self.temperature = temperature
-        self.pressure = pressure
-        self.humidity = humidity
+    def data():
+        data = pd.read_csv("./test_data/data.csv")
+        return data
+
+    def attributes(data):
+        id = data["id"]
+        timestamp = data["timestamp"]
+        temperature = data["temperature"]
+        pressure = data["pressure"]
+        humidity = data["humidity"]
+        return id, timestamp, temperature, pressure, humidity
     
-    # describe the test dataframe using the pandas built in libary
     def dataset_describe(testdata):
         desc = testdata.describe()
         return desc
     
     # implementing descriptive statistics with numpy
     def descriptive_statistics(temperature, pressure, humidity):
+        DS = {avg_temperature, avg_pressure, avg_humidity, med_temperature, med_pressure, med_humidity}
         avg_temperature = np.average(temperature)
         avg_pressure = np.average(pressure)
         avg_humidity = np.average(humidity)
         med_temperature = np.median(temperature)
         med_pressure = np.median(pressure)
         med_humidity = np.median(humidity)
-        return avg_temperature, avg_pressure, avg_humidity, med_temperature, med_pressure, med_humidity
+        return DS
+    
+    print(DS)
 
