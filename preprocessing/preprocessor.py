3663b31091d567f6e91a639189b9688d230d8972
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-16 12:55:50 +0200

    preprocessing

---
+++ preprocessing/preprocessor.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+testdata = pd.read_csv("../test_data/data.csv")
+
+class Attributes():
+    def attributes(self, testdata):
+        id = testdata["id"]
+        timestamp = testdata["timestamp"]
+        temperature = testdata["temperature"]
+        pressure = testdata["pressure"]
+        humidity = testdata["humidity"]
+        return id, timestamp, temperature, pressure, humidity
+
+# Create an instance of the Attributes class
+attr = Attributes()
+# Call the attributes method with testdata and store the returned values
+id, timestamp, temperature, pressure, humidity = attr.attributes(testdata)
+# Print the values
+print(id, timestamp, temperature, pressure, humidity)
