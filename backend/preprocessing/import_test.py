90c913e6d11a4f1ad76ea5d94eed8c7ea40f517b
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-17 14:18:35 +0200

    Update import_test.py

--- backend/preprocessing/import_test.py
+++ backend/preprocessing/import_test.py
@@ -1,10 +1,10 @@
 import bme280
-#import smbus2
+import smbus2
 
 # configuration of hardware ports
-port = 1
+port = 1 # potentially can be 0 if the RPi is older
 address = "0x76"
-"""bus = smbus2.SMBus(port)
+bus = smbus2.SMBus(port)
 
 # loading calibration params and reading a single line of data
 calibration_params = bme280.load_calibration_params(bus, address)
@@ -18,4 +18,4 @@ print(data.pressure)
 print(data.humidity)
 
 # string representation of the data
-print(data)"""
+print(data)
