a766397a33900ed133c339b5725bddedf5bfcab3
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-17 12:33:20 +0200

    Update west_front.py

--- frontend/west_front.py
+++ frontend/west_front.py
@@ -4,12 +4,25 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-# fetching the plot endpoint from the backend
-response = requests.get('http://localhost:5000/plot')
-data = response.json()
+# setting up page title and formatting config
+st.set_page_config(page_title="WEST: Weather Station with RaspberryPI", page_icon=':sunny:')
+st.title("WEST: Weather Station with RaspberryPI")
 
-# converting the base64 encoded object into an image
-descstatplot_data = base64.b64decode(data['plot_data'])
-descstatplot = Image.open(BytesIO(descstatplot_data))
+# adding the sidebar menu
+menu_options = ["Home", "Descriptive Statistics"]
+selected_option = st.sidebar.selectbox('Menu', menu_options)
 
-st.image(descstatplot)
+if selected_option == "Home":
+    # displaying a welcome screen on home
+    st.write("Welcome to the WEST project: Weather Station with RaspberryPI and Bosch BME.280 sensor!")
+elif selected_option == "Descriptive Statistics":
+    # fetching the plot endpoint from the backend
+    response = requests.get('http://localhost:5000/plot')
+    data = response.json()
+
+    # converting the base64 encoded object into an image
+    descstatplot_data = base64.b64decode(data['plot_data'])
+    descstatplot = Image.open(BytesIO(descstatplot_data))
+
+    st.subheader('Average and Median values')
+    st.image(descstatplot)
