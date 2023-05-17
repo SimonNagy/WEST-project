42cd2eb9c890a49d389d13aa090173aeb39bb530
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-17 14:42:08 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from analysis import analysis as an
 from preprocessing import preprocessor as prep
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
+from sensor import bme_module
 
 """
 Integration of the modules
@@ -15,3 +20,17 @@ id, timestamp, temperature, pressure, humidity = attr.attributes(testdata)
 
 ds = an.descriptive_statistics()
 descriptive_statistics = ds.calculation(temperature, pressure, humidity)
+
+"""
+Implementing the GCP Firestore communication channel, and the data upload mechanism. Steps 
+of development:
+1. initializing the GCP connection, credentials, and the client
+2. declaration of the upload_sensor_readings function
+"""
+
+cred = credentials.Certificate('TODO: path to service account')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def upload_sensor_readings():
+    # creating an instance of the sensor module
