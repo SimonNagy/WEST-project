b724e2ac73f5757de2b33da5875eb095952e299a
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-17 12:48:43 +0200

    Update plot.py

--- backend/plot.py
+++ backend/plot.py
@@ -8,6 +8,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import app
 
+# defining descriptive statistics plot
 def descstatplot():
     data = app.descriptive_statistics
 
