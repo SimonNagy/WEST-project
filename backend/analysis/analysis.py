ae3db929d13f7fff48b197c8700f1d218378a3ad
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-18 10:55:48 +0200

    Update analysis.py

--- backend/analysis/analysis.py
+++ backend/analysis/analysis.py
@@ -8,28 +8,38 @@ class data_head():
 
 class descriptive_statistics():
 
-    # data is an object, which is fetched from GCP in the app.py module
+    # :data: is an object, which is fetched from GCP in the :app.py: module
     def dataset_describe(self, data):
         desc = data.describe()
         return desc
     
     # implementing descriptive statistics with numpy
     @staticmethod
-    def average_and_median(temperature, pressure, humidity):
+    def average_and_median(self, data):
+        
+        # making objects to cover attributes to analyze
+        temperature = data["temperature"]
+        pressure = data["pressure"]
+        humidity = data["humidity"]
+
         avg_temperature = np.average(temperature)
         avg_pressure = np.average(pressure)
         avg_humidity = np.average(humidity)
         med_temperature = np.median(temperature)
         med_pressure = np.median(pressure)
         med_humidity = np.median(humidity)
-        ds = {"avg_temperature": avg_temperature, 
-              "avg_pressure": avg_pressure,
-               "avg_humidity": avg_humidity, 
-               "med_temperature": med_temperature,
-                "med_pressure": med_pressure, 
-                "med_humidity": med_humidity
-            }
-        return ds
+
+        # returning results named :avg_and_med:
+        avg_and_med = {
+            "avg_temperature": avg_temperature,
+            "avg_pressure": avg_pressure,
+            "avg_humidity": avg_humidity,
+            "med_temperature": med_temperature,
+            "med_pressure": med_pressure,
+            "med_humidity": med_humidity
+        }
+        
+        return avg_and_med
     
     @staticmethod
     def min_and_max(temperature, pressure, humidity):
